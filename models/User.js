e8ed69c9a2ec3f8c0d949c03af230eaa912cbf9c
Aminu Abubakar <aminuabubakar6361@gmail.com>
2026-07-27 09:37:28 +0100

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -6,6 +6,7 @@ const userSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true, trim: true, lowercase: true },
     email: { type: String, required: true, unique: true, trim: true, lowercase: true },
     passwordHash: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
